65538e94877454478a2151af5b61b7449d727a51
Sainath <sainatharao@gmail.com>
2021-08-26 23:33:08 +0530

    Tickets Updated

--- src/components/Tickets/ProjectCards.js
+++ src/components/Tickets/ProjectCards.js
@@ -1,20 +1,33 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { BiLinkExternal } from "react-icons/bi";
-
+import { BiCalendarCheck } from "react-icons/bi";
+import { GoLocation } from "react-icons/go"
+import price from '../../Assets/Tickets/price.svg';
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title style={{ textAlign: "left", color: "#C36EEC" }} >{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.link} target="_blank">
-          <BiLinkExternal /> &nbsp;
-          View Ticket
+        <Card.Text style={{ textAlign: "justify" }}>
+          <BiCalendarCheck style={{ marginRight: "10px", width: "25px", height: "25px" }} />
+          {props.datetime}
+        </Card.Text>
+
+        <Card.Text style={{ textAlign: "justify" }}>
+          <GoLocation style={{ marginRight: "10px", width: "25px", height: "25px" }} />
+          {props.location}
+        </Card.Text>
+        <Card.Text style={{ textAlign: "justify" }}>
+          <img src={price} alt="price" style={{ marginRight: "10px", width: "25px", height: "25px" }} />
+          {props.price}
+        </Card.Text>
+        <Button className="buy-btn" variant="primary" href={props.link} target="_blank" style={{ marginTop: "10px", width: "100px" }}>
+          Buy
         </Button>
       </Card.Body>
     </Card>
--- src/components/Tickets/Tickets.js
+++ src/components/Tickets/Tickets.js
@@ -4,8 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
 import leaf from "../../Assets/Tickets/leaf.png";
-import emotion from "../../Assets/Tickets/emotion.jpeg";
-import editor from "../../Assets/Tickets/codeEditor.png";
+import ticket2 from "../../Assets/Tickets/ticket2.png";
 
 function Tickets() {
   return (
@@ -15,44 +14,88 @@ function Tickets() {
         <h1 className="project-heading">
           Explore <strong className="purple">Tickets </strong>
         </h1>s
-        <p style={{ color: "white" }}>
+        <p style={{ color: "white", fontSize: "22px" }}>
           Top Selling Tickets on the market
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ticket2}
               isBlog={false}
               title="Ticket #01"
-              description="First of all, the total value of projects staked on Solana recently exceeded $2 billion. It has also become the token of choice for cryptocurrency billionaire Sam Bankman-Fried, the founder of FTX, the fifth-largest cryptocurrency in the world. As a result, Solana's gains did not lose their momentum even as the network suffered its first-ever rug pull."
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
+              link="https://github.com/soumyajit4419/Plant_AI"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ticket2}
+              isBlog={false}
+              title="Ticket #01"
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
+              link="https://github.com/soumyajit4419/Plant_AI"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ticket2}
+              isBlog={false}
+              title="Ticket #01"
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
+              link="https://github.com/soumyajit4419/Plant_AI"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ticket2}
+              isBlog={false}
+              title="Ticket #01"
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
+              link="https://github.com/soumyajit4419/Plant_AI"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ticket2}
+              isBlog={false}
+              title="Ticket #01"
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
+              link="https://github.com/soumyajit4419/Plant_AI"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ticket2}
+              isBlog={false}
+              title="Ticket #01"
+              description="A long description about the event like artists, etc or other details in about 3-4 lines."
+              datetime="5:40 | 23-06-2003"
+              location="Afghanisthan"
+              price="500 SOL"
               link="https://github.com/soumyajit4419/Plant_AI"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Ticket #02"
-              description="First of all, the total value of projects staked on Solana recently exceeded $2 billion. It has also become the token of choice for cryptocurrency billionaire Sam Bankman-Fried, the founder of FTX, the fifth-largest cryptocurrency in the world. As a result, Solana's gains did not lose their momentum even as the network suffered its first-ever rug pull."
-              link="https://github.com/soumyajit4419/Plant_AI"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Ticket #03"
-              description="First of all, the total value of projects staked on Solana recently exceeded $2 billion. It has also become the token of choice for cryptocurrency billionaire Sam Bankman-Fried, the founder of FTX, the fifth-largest cryptocurrency in the world. As a result, Solana's gains did not lose their momentum even as the network suffered its first-ever rug pull."
-              link="https://github.com/soumyajit4419/Plant_AI"
-            />
-          </Col>
 
 
         </Row>
       </Container>
-    </Container>
+    </Container >
   );
 }
 
